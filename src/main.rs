458e4d3a753298a13e4c820ed9da54290528c1c2
Anthony Goncharenko <amg568@drexel.edu>
2023-10-29 18:33:34 -0400

    simplified command parsing

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,26 @@ use std::{env, process::Command};
 #[derive(Debug)]
 enum Error {
     MissingCLIArgument(String),
-    IncorrectCommand(String),
+    InvalidCommand(String),
     ShellCommand(String),
-}
-#[derive(Debug)]
-enum ScriptCommand {
-    Run,
-    Test,
-    Build,
-}
-impl From<String> for ScriptCommand {
-    fn from(value: String) -> Self {
-        if value == *"run" {
-            Self::Run
-        } else if value == *"test" {
-            Self::Test
-        } else if value == *"build" {
-            Self::Build
-        } else {
-            panic!("Invalid Command Reached. Must be run|test|build")
-        }
-    }
-}
-impl ToString for ScriptCommand {
-    fn to_string(&self) -> String {
-        match self {
-            ScriptCommand::Run => "run".to_string(),
-            ScriptCommand::Test => "test".to_string(),
-            ScriptCommand::Build => "build".to_string(),
-        }
-    }
 }
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     // dbg!(&args);
     if args.len() as i32 != 3 {
-        let msg = "Missing CLI Arguments. Usage => cargo test|run|build folder";
+        let msg = "Usage :: cargo test|run|build subdir_name";
         return Err(Error::MissingCLIArgument(msg.to_string()));
     }
 
-    let command = ScriptCommand::from(match args.get(1) {
-        Some(t) => t.to_owned(),
-        None => {
-            return Err(Error::IncorrectCommand(
-                "Missing Command Argument!".to_string(),
-            ))
-        }
-    });
+    let command = match args.get(1) {
+        Some(t) => match t.as_str() {
+            "run" | "test" | "build" => t,
+            _ => return Err(Error::InvalidCommand("Must be run|test|build".to_string())),
+        },
+        None => unreachable!(),
+    };
+
     let arg2 = match args.get(2) {
         Some(t) => t,
         None => {
@@ -61,7 +33,7 @@ fn main() -> Result<(), Error> {
     };
 
     let output = match Command::new("cargo")
-        .arg(command.to_string())
+        .arg(command)
         .current_dir(arg2)
         .output()
     {
